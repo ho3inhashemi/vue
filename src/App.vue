db1d7311dc06cfff99e1765caabd7e1929566b9e
ho3inhashemi <ho3inhashemi@gmail.com>
2022-01-14 00:14:43 +0330

    added modal component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
 <h1>{{title}} text</h1>
+<modal />
+<input type="text" ref="name">
+<button @click="handleEvent">click</button>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: {Modal},
   data(){
     return{
       title: 'this is a test'
+    }
+  },
+  methods: {
+    handleEvent(){
+      console.log('event')
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('mahdi')
     }
   }
 }
@@ -24,6 +37,8 @@ export default {
 }
 
 h1{
-  color: red;
+  border-bottom: solid 1px #ccc ;
+  display: inline-block ;
+  padding-bottom: 10px ;  
 }
 </style>
